90dcd102a428e7762feabe6ef059e5ee806f1dff
AmrAmin19 <100246329+AmrAmin19@users.noreply.github.com>
2024-08-18 15:59:58 +0300

    buttom Appbar

--- app/src/main/java/com/example/ratatouilleapp/View/MainActivity.java
+++ app/src/main/java/com/example/ratatouilleapp/View/MainActivity.java
@@ -10,11 +10,15 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.ratatouilleapp.Model.Firebase.FireBaseAuthHandler;
 import com.example.ratatouilleapp.Model.Repo.Irepo;
 import com.example.ratatouilleapp.Model.Repo.Respiratory;
 import com.example.ratatouilleapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //  BottomAppBar bottomAppBar=findViewById(R.id.)
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        // Set up navigation with the BottomNavigationView
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
         model=Respiratory.getInstance(this,new FireBaseAuthHandler());
         button=findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
